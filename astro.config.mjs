deddac98b3149d085af9e5892927cd367b13e03c
Bryant Park <bryantpark04@gmail.com>
2024-12-19 20:08:07 -0500

    Add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://engrc-3025.bryantpark.dev'
+  site: 'https://engrc-3025.bryantpark.dev',
+  integrations: [react()]
 });
